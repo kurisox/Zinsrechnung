3453be38d846ba1588e3e035585bf3b19bd0b45e
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-03-17 10:02:59 +0100

    jahre hinzugefügt und to-string überarbeitet

--- Zinsrechnung/Zinsobjekt.cs
+++ Zinsrechnung/Zinsobjekt.cs
@@ -3,12 +3,13 @@ namespace Zinsrechnung
     public class Zinsobjekt
     {
         public double kapitlal {get; set;}
+        public double jahre{get;set;}
         public double zinssatz {get; set;}
         public double endkapital {get; set;}
 
         public override string ToString()
         {
-            return "Kapital: " + kapitlal + "€, Zinssatz: " + zinssatz + "%, Endkapital: " + endkapital + "€";
+            return "Kapital: " + kapitlal + "€, Zinssatz: " + zinssatz + "%, Laufzeit: " + jahre + " Jahre, Endkapital: " + endkapital +"€";
         }
     }
 }
