5353a640f0f3e6c4435b4420b9ff43984fe8af92
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-03-17 10:03:36 +0100

    formeln überarbeitet

--- Zinsrechnung/Calculator.cs
+++ Zinsrechnung/Calculator.cs
@@ -3,15 +3,22 @@ namespace Zinsrechnung
     public class Calculator
     {
         public double calcEndkapital (Zinsobjekt zinsobjekt){
-            return zinsobjekt.kapitlal * (1 + (zinsobjekt.zinssatz / 100));
+            return zinsobjekt.kapitlal * (1+ (zinsobjekt.zinssatz * zinsobjekt.jahre) / 100);
+            //return zinsobjekt.kapitlal*Math.Pow(1 + (zinsobjekt.zinssatz/100), zinsobjekt.jahre);
         }
 
         public double calcStartkapital (Zinsobjekt zinsobjekt){
-            return Math.Round(zinsobjekt.endkapital / (1 + (zinsobjekt.zinssatz / 100)));
+            return zinsobjekt.endkapital / (1+ (zinsobjekt.zinssatz * zinsobjekt.jahre) / 100);
+            //return zinsobjekt.endkapital / Math.Pow(1 + zinsobjekt.zinssatz / 100, zinsobjekt.jahre);
         }
 
         public double calcZinssatz (Zinsobjekt zinsobjekt){
-            return ((zinsobjekt.endkapital - zinsobjekt.kapitlal) / zinsobjekt.kapitlal) * 100;
+            return ((zinsobjekt.endkapital / zinsobjekt.kapitlal - 1) * 100) / zinsobjekt.jahre;
+            //return 100*(Math.Pow(zinsobjekt.endkapital / zinsobjekt.kapitlal, 1 / zinsobjekt.jahre) -1);
+        }
+
+        public double calcJahre( Zinsobjekt zinsobjekt){
+            return ((zinsobjekt.endkapital / zinsobjekt.kapitlal - 1) * 100) / zinsobjekt.zinssatz;
         }
     }
 }
